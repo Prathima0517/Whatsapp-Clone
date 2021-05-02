17db94fb27e35567a3309935a2810544a8322d2f
Prathima0517 <prathima1269@gmail.com>
2021-05-02 16:52:37 +0530

    initial commit

---
+++ lib/whatsapp_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class WhatsappHome extends StatefulWidget {
+  @override
+  _WhatsappHomeState createState() => _WhatsappHomeState();
+}
+
+class _WhatsappHomeState extends State<WhatsappHome> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Whatsapp"))
+      
+    );
+  }
+}
